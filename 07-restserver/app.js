c91d87534d19253e3451a2411f348e1282dd5bd4
Sebastian Gonzalez <seba_gonza_@hotmail.com>
2020-05-31 22:27:39 -0400

    app

--- 07-restserver/app.js
+++ 07-restserver/app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-require('./config/config');
+require('./server/config/config');
 const PORT = process.env.PORT;
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
     // parse application/json
 app.use(bodyParser.json())
-app.use(require('./routes/usuario'))
+app.use(require('./server/routes/usuario'))
 
 app.listen(PORT, () => {
     console.log(`Escuchando puerto ${PORT}`);
